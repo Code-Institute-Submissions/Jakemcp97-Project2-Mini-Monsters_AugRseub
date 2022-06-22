84ae4ae4681c0438efe46823ca91ececc308c333
Jakemcp97 <103071250+Jakemcp97@users.noreply.github.com>
2022-06-22 23:09:53 +0100

    adding attack function

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ var heroStats = document.getElementById('heroStats');
 var geraltHp = document.getElementById('geraltHp');
 var pookaHp = document.getElementById('pookaHp';)
 
+//health variables
+var heroHp = 100;
+var enemyHp = 100;
+
 //function initiating the game
 function startFight(){
 bottomRow.innerHTML="Choose your action to defeat the Pooka!";
@@ -16,5 +20,14 @@ for(var x = 0; x < stats.length; x++){
 
 //basic attack function
 function slash(){
-
+    var hitChance = Math.round(Math.random()*10);
+    if(hitChange < 7 ){
+        var dmg = Math.round(Math.random()*10)*10;
+        enemyHp -= dmg;
+        var enemyHpBarWidth = 
+        pookaHp.style.width = 
+        bottomRow.innerHTML = "You slashed the Pooka causing "+ dmg +" damage! The pooka has "+ enemyHp + "hit points remaining!";
+    }else{
+        bottomRow.innerHTML = "The Pooka dodged your attack!";
+    }
 }
